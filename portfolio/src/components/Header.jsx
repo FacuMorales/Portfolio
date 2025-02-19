6ff25c2bc5b3bffd148ff9b9536ba4211368ea0c
FacuMorales <facu44515119@gmail.com>
2025-02-19 12:43:33 -0300

    Header finalizado 1.0

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -2,25 +2,39 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full flex justify-between items-center py-4 px-6 bg-white shadow-md">
-      {/* Logo o Nombre */}
-      <h1 className="text-2xl font-bold">FM</h1>
+    <header className="w-full py-4 px-8 shadow-md">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between">
+        {/* Logo o Nombre */}
+        <h1 className="text-2xl font-bold ml-[150px]">FM</h1>
 
-      {/* Enlaces */}
-      <nav className="md:flex space-x-6">
-        <a href="#projects" className="text-gray-700 hover:text-black">Proyectos</a>
-        <a href="#about" className="text-gray-700 hover:text-black">Sobre mí</a>
-        <a href="#experience" className="text-gray-700 hover:text-black">Experiencia</a>
-      </nav>
+        {/* Enlaces */}
+        <nav className="md:flex space-x-6 ml-[120px]">
+          <a href="#projects" className="text-gray-700 hover:text-black">Proyectos</a>
+          <a href="#about" className="text-gray-700 hover:text-black">Sobre mí</a>
+          <a href="#experience" className="text-gray-700 hover:text-black">Experiencia</a>
+        </nav>
 
-      {/* Botones */}
-      <div className="flex space-x-4">
-        <a href="/cv.pdf" className="md:block border px-4 py-2 rounded-lg hover:bg-gray-100">
-          Ir a CV
-        </a>
-        <a href="#contact" className="bg-yellow-400 px-4 py-2 rounded-lg text-white hover:bg-yellow-500">
-          Contáctame
-        </a>
+        {/* Botones */}
+        <div className="space-x-4 flex items-center">
+          <a
+            href="#cv"
+            className="relative border rounded-3xl px-12 py-4 font-bold flex items-center text-gray-700 hover:text-black hover:bg-gray-100 transition group"
+          >
+            Ir a CV
+            <span className="ml-1 inline-block transition-transform group-hover:translate-x-2">
+              ➜
+            </span>
+          </a>
+          <a
+            href="#contacto"
+            className="relative flex items-center px-12 py-4 bg-yellow-300 rounded-3xl font-bold transition hover:bg-yellow-400 group"
+          >
+            Contáctame
+            <span className="ml-1 inline-block transition-transform group-hover:translate-x-2">
+              ➜
+            </span>
+          </a>
+        </div>
       </div>
     </header>
   );
